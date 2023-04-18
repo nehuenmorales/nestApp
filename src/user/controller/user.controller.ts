f9993950404b7585873dc5dd7ef90dd2d282d1f4
nehuenmorales <nehuenmorales640@gmail.com>
2023-04-18 14:53:26 -0300

    getUsers

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { UserService } from '../service/user.service';
 
 @Controller('controller')
-export class UserController {}
+export class UserController {
+    constructor(private userService : UserService){}
+
+    @Get()
+    getUsers(): Promise<User[]>{
+        return this.userService.getUsers()
+    }
+}
