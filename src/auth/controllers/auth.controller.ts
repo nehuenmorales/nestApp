b35fcfa1bcd0aff3b67b76451fabb428145ba150
nehuenmorales <nehuenmorales640@gmail.com>
2023-04-21 13:12:47 -0300

    auth login

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Param  } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
 import { threadId } from 'worker_threads';
+import { LoginAuthDto } from '../dto/auth-login.dto';
 import { RegisterAuthDto } from '../dto/auth-register.dto';
 import { AuthService } from '../service/auth.service';
 
@@ -11,5 +12,10 @@ export class AuthController {
     @Post('register')
     registerUser(@Body() newUser : RegisterAuthDto): Promise<User>{
         return this.authService.register(newUser);
+    }
+
+    @Post('login')
+    loginUser(@Body() loginUser : LoginAuthDto): Promise<any>{
+        return this.authService.login(loginUser)
     }
 }
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { LoginAuthDto } from '../dto/auth-login.dto';
 import { RegisterAuthDto } from '../dto/auth-register.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectRepository(User) private userRepo : Repository<User>){}
+    constructor(@InjectRepository(User) private userRepo : Repository<User>, private jwtService : JwtService){}
 
 
     async register(user : RegisterAuthDto){
@@ -24,7 +25,20 @@ export class AuthService {
     }
 
     async login(user : LoginAuthDto){
+        const {email, password} = user;
+        const findUser = await this.userRepo.findOne({where:{email}})
 
+        if(!findUser) throw new HttpException("user_not_found", 404) 
+
+        const payload = {email: findUser.email};
+        const token = await this.jwtService.sign(payload);
+
+        const data = {
+            user: findUser,
+            token
+        }
+
+        return data;
     }
 
 }
