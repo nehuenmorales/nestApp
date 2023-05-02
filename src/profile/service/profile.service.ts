bd01ae0429d49748ee1474ba2dd727e215b2feba
nehuenmorales <nehuenmorales640@gmail.com>
2023-05-02 11:52:36 -0300

    update profile

--- src/profile/service/profile.service.ts
+++ src/profile/service/profile.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Profile } from '../entities/profile.entity';
 
 @Injectable()
 export class ProfileService {
-    constructor  (@InjectRepository(Profile) private profileRepo : Repository<Profile>){}
+    constructor  (@InjectRepository(Profile) private profileRepo : Repository<Profile>,
+    @InjectRepository(User) private userRepo : Repository<User>){}
 
     async getAllProfile(){
         return await this.profileRepo.find();
@@ -16,7 +18,16 @@ export class ProfileService {
     }
 
     async create(objet : any){
+        const user = await this.userRepo.find({where:{id: objet.idUser}})
         const newProfile =  await this.profileRepo.create(objet);
         return await this.profileRepo.save(newProfile);
+    }
+
+    async update(id: number, objet: any ){
+        const existProfile = await this.profileRepo.find({where:{id}}) 
+        if(!existProfile){throw new HttpException("this profile does not exist", 404)}
+        const updateProfile = Object.assign(existProfile, objet);
+
+        return this.profileRepo.save(updateProfile);
     }
 }
